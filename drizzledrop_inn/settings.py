0e3a34ddfe95def48e0be508bafeea339838142a
AADITYA RP <150111578+AadityaRP@users.noreply.github.com>
2026-03-02 12:09:08 +0530

    Update settings.py
    Updated the MySQL password environment variable to be empty.

--- drizzledrop_inn/settings.py
+++ drizzledrop_inn/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": os.getenv("MYSQL_DATABASE", "drizzledrop_inn"),
         "USER": os.getenv("MYSQL_USER", "root"),
-        "PASSWORD": os.getenv("MYSQL_PASSWORD", "Aadi26"),
+        "PASSWORD": os.getenv("MYSQL_PASSWORD", ""),
         "HOST": os.getenv("MYSQL_HOST", "127.0.0.1"),
         "PORT": os.getenv("MYSQL_PORT", "3306"),
         "OPTIONS": {
